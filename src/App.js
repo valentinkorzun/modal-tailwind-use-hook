8428451d6423bd664783e4c06d51af7a636a925e
Valentin Korzun <valentin.korzunn@gmail.com>
2022-07-05 14:04:19 +0300

    TEST CLOUDFRONT CACHE

--- src/App.js
+++ src/App.js
@@ -2,19 +2,65 @@ import { Modal, ModalWrapper } from "./components";
 import { useModal } from "./hooks";
 import React from "react";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function Dashboard() {
+  return <h2>This is the Dashboard</h2>;
+}
+
 function App() {
-  const { close, open, isOpen, ref } = useModal();
-
   return (
-    <>
-      <ModalWrapper ref={ref} isOpen={isOpen} onClose={close}>
-        <Modal close={close} />
-      </ModalWrapper>
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+            </ul>
+          </nav>
 
-      <button onClick={open} className="bg-amber-300 p-5 m-5">
-        Open My Modal
-      </button>
-    </>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
